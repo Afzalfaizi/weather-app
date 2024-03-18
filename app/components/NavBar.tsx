ba0831b54b2f79abdb35dc806edb4ecceeb4b1fb
afzalfaizi <afzaalm993@gmail.com>
2024-03-18 20:46:13 +0500

    Weather App Using Weather API

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -6,22 +6,25 @@ import SearchBox from "./SearchBox";
 
 const NavBar = () => {
   return (
-    <nav className=" shadow-sm sticky top-0 left-0 z-50 bg-white">
-      <div className=" h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
-        <p className=" flex items-center justify-center gap-2">
-          <h2 className=" text-gray-500 text-3xl">Current Weather</h2>
-          <IoSunny className=" text-3xl mt-1 text-yellow-400" />{" "}
-        </p>
-        <section className=" flex gap-2 items-center">
-          <IoMdLocate className=" text-2xl text-gray-400 hover:opacity-80 cursor-pointer" />
-          <MdOutlineLocationOn className=" text-3xl" />
-          <p className=" text-slate-900/80 text-sm"> Pakistan </p>
-          <div>
-            <SearchBox />
-          </div>
-        </section>
-      </div>
-    </nav>
+    <>
+      {" "}
+      <nav className=" shadow-sm sticky top-0 left-0 z-50 bg-white">
+        <div className=" h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
+          <p className=" flex items-center justify-center gap-2">
+            <h1 className=" text-gray-500 text-3xl">Current Weather</h1>
+            <IoSunny className=" text-3xl mt-1 text-yellow-400" />{" "}
+          </p>
+          <section className=" flex gap-2 items-center">
+            <IoMdLocate className=" text-2xl text-gray-400 hover:opacity-80 cursor-pointer" />
+            <MdOutlineLocationOn className=" text-3xl" />
+            <p className=" text-slate-900/80 text-sm"> Pakistan </p>
+            <div>
+              <SearchBox />
+            </div>
+          </section>
+        </div>
+      </nav>
+    </>
   );
 };
 
--- app/components/SearchBox.tsx
+++ app/components/SearchBox.tsx
@@ -1,11 +1,23 @@
+import { cn } from '@/utils/cn';
 import React from 'react'
 import { IoSearch } from "react-icons/io5";
 
-const SearchBox = () => {
+type Props = {
+  className? : string,
+  value:string
+  onChange:React.ChangeEventHandler<HTMLInputElement> | undefined
+  onSubmit:React.FormEventHandler<HTMLFormElement> | undefined
+};
+
+const SearchBox = (props: Props) => {
   return (
-    <form className=' flex relative items-center justify-center h-10'>
+    <form
+     onSubmit={props.onSubmit}
+      className={cn(' flex relative items-center justify-center h-10', props.className)}>
         <input
          type="text"
+         value={props.value}
+         onChange={props.onChange}
          placeholder=' search location..'
          className=' px-4 py-2 w-[230px] border border-gray-50 rounded-l-md focus:outline-none focus:border-blue-500 h-full' />
         <button className=' px-4 py-[9px] bg-blue-500 text-white- rounded-r-md focus:outline-none hover:bg-blue-400 whitespace-nowrap h-full '>
